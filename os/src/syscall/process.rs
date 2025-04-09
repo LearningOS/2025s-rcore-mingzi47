307302617b04303d0283976bbc7a3d3002ee36a8
mingzi <1409429504@qq.com>
2025-04-09 12:06:47 +0800

    implement sys_get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,10 +1,11 @@
 use crate::{
     fs::{open_file, OpenFlags},
-    mm::{translated_ref, translated_refmut, translated_str},
+    mm::{translated_ref, translated_refmut, translated_byte_buffer, translated_str},
     task::{
         current_process, current_task, current_user_token, exit_current_and_run_next, pid2process,
         suspend_current_and_run_next, SignalFlags,
     },
+    timer::get_time_us,
 };
 use alloc::{string::String, sync::Arc, vec::Vec};
 
@@ -151,12 +152,41 @@ pub fn sys_kill(pid: usize, signal: u32) -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     trace!(
         "kernel:pid[{}] sys_get_time NOT IMPLEMENTED",
         current_task().unwrap().process.upgrade().unwrap().getpid()
     );
-    -1
+    let token = current_user_token();
+    let ptr = ts as *const u8;
+    let len = mem::size_of::<TimeVal>();
+
+    // 获取应用空间内存
+    let buffers = translated_byte_buffer(token, ptr, len);
+    let us = get_time_us();
+
+    // 将要写入的数据转换为字节流，方便写入
+    let tmp = TimeVal {
+        sec : us / 1_000_000,
+        usec : us % 1_000_000,
+    };
+    // https://stackoverflow.com/questions/28127165/how-to-convert-struct-to-u8
+    let tmp_data = unsafe {
+        core::slice::from_raw_parts(
+            (&tmp as *const TimeVal) as *const u8,
+            len,
+        )
+    };
+
+    // 写入应用空间内存
+    let mut tmp_i = 0;
+    for buffer in buffers {
+        for b in buffer {
+            *b = tmp_data[tmp_i];
+            tmp_i+=1;
+        }
+    }
+    0
 }
 
 /// mmap syscall
