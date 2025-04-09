aa347dcc041d71a8c8d37a73b018267338369cb8
mingzi <1409429504@qq.com>
2025-04-09 12:06:47 +0800

    implement sys_get_time

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,7 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+use core::mem;
+
+use crate::{mm::translated_byte_buffer, task::{change_program_brk, current_user_token, exit_current_and_run_next, suspend_current_and_run_next}, timer::get_time_us};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -25,9 +27,38 @@ pub fn sys_yield() -> isize {
 /// YOUR JOB: get time with second and microsecond
 /// HINT: You might reimplement it with virtual memory management.
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
-pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
+pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    let token = current_user_token();
+    let ptr = ts as *const u8;
+    let len = mem::size_of::<TimeVal>();
+
+    // 获取应用空间内存
+    let buffers = translated_byte_buffer(token, ptr, len);
+    let us = get_time_us();
+
+    // 将要写入的数据转换为字节流，方便写入
+    let tmp = TimeVal {
+        sec : us / 1_000_000,
+        usec : us % 1_000_000,
+    };
+    // https://stackoverflow.com/questions/28127165/how-to-convert-struct-to-u8
+    let tmp_data = unsafe {
+        core::slice::from_raw_parts(
+            (&tmp as *const TimeVal) as *const u8,
+            len,
+        )
+    };
+
+    // 写入应用空间内存
+    let mut tmp_i = 0;
+    for buffer in buffers {
+        for b in buffer {
+            *b = tmp_data[tmp_i];
+            tmp_i+=1;
+        }
+    }
+    0
 }
 
 /// TODO: Finish sys_trace to pass testcases
